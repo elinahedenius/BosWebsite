6f9c2f90a88b98b9f9f207536c49386b1ea46e28
Bo Hedenius <elina.hadenius@learnet.se>
2020-01-05 10:19:59 +0100

    started translating carol

---
+++ carol.js
@@ -0,0 +1,7 @@
+var inpatient = 0;
+var blankinput = 0;
+var caroldead = false;
+
+//Collecting names
+document.write("Hello, my name is Carol, what is your name?")
+var name = prompt("Your name: ")
